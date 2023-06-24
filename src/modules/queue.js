1cedbb90703713f3ab862d910a7d9102b22c1688
Azam Gulov <gulovazam@gmail.com>
2023-06-24 15:12:43 +0500

    Add queue class

--- src/modules/queue.js
+++ src/modules/queue.js
@@ -1,1 +1,19 @@
+class Queue {
+  constructor(start) {
+    this.queue = [start];
+  }
 
+  enqueue(value) {
+    this.queue.push(value);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  isEmpty() {
+    return !this.queue.length;
+  }
+}
+
+export default Queue;

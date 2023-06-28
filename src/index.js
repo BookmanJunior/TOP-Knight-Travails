339e4dd9bd47a156defcd22a0439a89e468468e0
Azam Gulov <gulovazam@gmail.com>
2023-06-28 18:57:09 +0500

    Remove console logs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Knight from "./modules/knight.js";
 
 const knight = new Knight();
 
-console.log(knight.moves([0, 0], [0, 0]));
-console.log(knight.moves([0, 0], [3, 3]));
-console.log(knight.moves([3, 3], [0, 0]));
-console.log(knight.moves([3, 3], [4, 3]));
+knight.moves([0, 0], [0, 0]);
+knight.moves([0, 0], [3, 3]);
+knight.moves([3, 3], [0, 0]);
+knight.moves([3, 3], [4, 3]);
